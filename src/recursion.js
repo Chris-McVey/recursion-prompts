a764b3da61ea5b5d7dc8547ca5609bca313af000
Chris McVey <chris.mcvey@gmail.com>
2021-01-04 18:50:36 -0800

    Finished through 3

--- src/recursion.js
+++ src/recursion.js
@@ -7,16 +7,41 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  if (n < 0) {
+    return null;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return (n * factorial(n - 1));
+  
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+  var arrayTemp = array.slice();
+  if (!arrayTemp.length) {
+    return 0;
+  }
+  return (arrayTemp.pop() + sum(arrayTemp));
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  var sum = 0;
+  if (!array.length) {
+    return 0;
+  }
+  array.forEach(function(item) {
+    if (!Array.isArray(item)) {
+      sum += item;
+    } else {
+    sum += arraySum(item)
+    }
+  });
+    return sum;
 };
 
 // 4. Check if a number is even.
